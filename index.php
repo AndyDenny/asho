02b12ed341b3253a19b3f3136927c23388f260e2
Andrey Denisov <andrey.denisov@tularegion.ru>
2025-03-12 17:14:13 +0300

    add styles and scripts files

--- index.php
+++ index.php
@@ -1,2 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>asho</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/mystyle.css">
+    <script src="assets/js/bootstrap.js"></script>
+    <script src="assets/js/jquery-3.7.1.min.js"></script>
+    <script src="assets/js/myscript.js"></script>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1 class='text-center'>Link Shorter</h1>
+            </div>
+        </div>
+    </div>    
+</body>
+</html>
 <?php
-phpinfo();
